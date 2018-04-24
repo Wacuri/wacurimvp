bdb2548dd27503c4a930103fefa153567c53874f
Keith Norman <keithnorm@gmail.com>
2018-04-23 21:39:08 -0700

    some cleanup

--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
 const dotenv = require('dotenv');
 
-console.log('DO CONFIG YO');
 dotenv.config();
 global.__CLIENT__ = false;
 global.__SERVER__ = true;
--- server/routes/api.js
+++ server/routes/api.js
@@ -78,8 +78,6 @@ router.get('/sessions/:room/connections/:connection/ready', async (req, res) => 
 
 router.get('/journeys', async (req, res) => {
   const readdirAsync = promisify(fs.readdir)
-  console.log('I AM', __dirname);
-  console.log('TRY DA READ', path.join('..', 'public/journeys'));
   const journeyFiles = (await readdirAsync(path.join(__dirname, '..', 'public/journeys'))).filter(file => {
     return path.extname(file) === '.mp3';
   }).map(file => {
