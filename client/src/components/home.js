798c3166b00774e37983db4e97924c76f87fdff1
Raphael Villas <raphael.villas@gmail.com>
2018-04-25 09:44:15 -0500

    Squash with previous commit: Pass the room name to the session link exposed to the user

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import GeneratorForm from './generator_form'
-import SessionInfo from './session_info'
 
 const Home = () => (
   <div className="home">
     <GeneratorForm />
-    <SessionInfo />
   </div>
 );
 
