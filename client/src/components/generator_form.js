04a0feb8f2324b2a022704c786870ddbf288ff73
Raphael Villas <raphael.villas@gmail.com>
2018-04-25 08:43:40 -0500

    Initial methods to convert a session title to a url friendly name to create a Wacuri url

--- client/src/components/generator_form.js
+++ client/src/components/generator_form.js
@@ -7,18 +7,32 @@ export default class GeneratorForm extends Component {
     this.state = {
       sessionLinkName: ''
     }
+
+    this.handleNameChange = this.handleNameChange.bind(this)
+    this.urlFriendlyName = this.urlFriendlyName.bind(this)
+    this.createSessionLink = this.createSessionLink.bind(this)
   }
 
   createSessionLink() {
-    console.log("*** createSessionLink")
+    console.log(this.urlFriendlyName(this.state.sessionLinkName))
+    return this.urlFriendlyName(this.state.sessionLinkName)
   }
+
+  handleNameChange(event) {
+    this.setState({sessionLinkName: event.target.value})
+  }
+
+  urlFriendlyName(name){
+    return name.replace(/\s+/g, '-').toLowerCase()
+  }
+
 
   render() {
       return(
         <div>
           <form>
             <h3>Give your room a name</h3>
-            <input type="text" id="session_link" />
+            <input type="text" id="session_link" onChange={this.handleNameChange} />
             <p>
               <input type="button" value="Create a link" onClick={this.createSessionLink} />
               &nbsp; or <a href='#'>Generate a name</a>
