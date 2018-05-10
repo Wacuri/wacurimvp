d222b21d810c58e8586eb418139440e9ae2a8889
Keith Norman <keithnorm@gmail.com>
2018-05-10 10:30:40 -0700

    remove some unused files

--- client/src/app.js
+++ client/src/app.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import Header from './components/header';
 import Home from './components/home';
-import ItemView from './containers/list_item_view';
 import Room from './components/Room';
 
 const App = () => (
@@ -10,7 +9,6 @@ const App = () => (
     <Header />
     <Route exact path="/" component={Home} />
 		<Route exact path="/:room" component={Room} />
-    <Route exact path="/view/:name" component={ItemView} />
   </div>
 );
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
 import {
   BrowserRouter,
 } from 'react-router-dom';
 import { AppContainer } from 'react-hot-loader';
-import reducers from './reducers/index';
 import App from './app';
 
 
@@ -15,19 +12,15 @@ Here we are getting the initial state injected by the server. See routes/index.j
  */
 const initialState = window.__INITIAL_STATE__; // eslint-disable-line
 
-const store = createStore(reducers, initialState);
-
 /*
 While creating a store, we will inject the initial state we received from the server to our app.
  */
 const render = (Component) => {
   ReactDOM.render(
     <AppContainer>
-      <Provider store={store}>
         <BrowserRouter>
           <Component />
         </BrowserRouter>
-      </Provider>
     </AppContainer>,
     document.getElementById('reactbody'),
   );
