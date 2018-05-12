e84d765f2779e408d20c858157bff403910f01d8
Raphael Villas <raphael.villas@gmail.com>
2018-05-12 18:03:30 -0500

    Cleanup EventMessage

--- client/src/components/event_message.js
+++ client/src/components/event_message.js
@@ -1,23 +1,19 @@
-import React, {Component} from 'react'
+import React from 'react'
 
-export default class EventMessage extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div>
-        {this.props.journeys.map((journey, index) => (
-          <p key={index}
-             style={{ backgroundColor: '#fc9', padding: '7px'}}>
-             A journey space has been created.
-             <br/>
-             <a href={"/" + journey.room} target="_blank">Join</a>
-             &nbsp;&nbsp;
-             <a href="#">Share</a></p>
-        ))}
-      </div>
-    );
-  }
+const EventMessage = (props) => {
+  return (
+    <div>
+      {props.journeys.map((journey, index) => (
+        <p key={index}
+           style={{ backgroundColor: '#fc9', padding: '7px'}}>
+           A journey space has been created.
+           <br/>
+           <a href={"/" + journey.room} target="_blank">Join</a>
+           &nbsp;&nbsp;
+           <a href="#">Share</a></p>
+      ))}
+    </div>
+  );
 }
+
+export default EventMessage
