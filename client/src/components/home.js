eccd29b6d7b8df8815e10ccaba9a015206aeec2a
Raphael Villas <raphael.villas@gmail.com>
2018-05-12 18:41:08 -0500

    A bit of cleanup on home.js

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -13,11 +13,9 @@ if (__CLIENT__) {
   window.state = state;
 }
 
-
 export default class Home extends Component {
   constructor(props) {
-    super(props)
-    state.activeJourneys = []
+    super(props);
 
     this.state = {
       streams: [],
@@ -33,26 +31,6 @@ export default class Home extends Component {
   componentDidMount() {
     const roomUrl = 'temp-home-location'
 
-    // let messageData = {
-    //   userName: "Bob",
-    //   description: "some text",
-    //   url: "http://www.news.google.com"
-    // }
-
-    // fetch("/api/sessions/temp-home-location", { body: '{}'}, {method: 'POST' })
-      //  fetch(`/api/sessions/${roomUrl}`, {
-      //   body: JSON.stringify(messageData), // must match 'Content-Type' header
-      //   cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-      //   credentials: 'same-origin', // include, same-origin, *omit
-      //   headers: {
-      //     'user-agent': 'Mozilla/4.0 MDN Example',
-      //     'content-type': 'application/json'
-      //   },
-      //   method: 'POST', // *GET, POST, PUT, DELETE, etc.
-      //   mode: 'cors', // no-cors, cors, *same-origin
-      //   redirect: 'follow', // manual, *follow, error
-      //   referrer: 'no-referrer'// *client, no-referrer
-      // })
     fetch(`/api/sessions/${roomUrl}`)
       .then(res => res.json())
       .then(json => {
@@ -62,17 +40,14 @@ export default class Home extends Component {
           sessionId: state.session.sessionId,
           token: state.session.token,
           onConnect: () => {
-            console.log('assigned connection to publisher', this.sessionHelper.session.connection);
             setTimeout(this.refreshSession, 1000);
           },
           onStreamsUpdated: streams => {
-            console.log('Current subscriber streams:', streams);
             this.setState({ streams });
           }
         });
         window.sh = this.sessionHelper;
         this.sessionHelper.session.on("connectionDestroyed", (event) => {
-          console.log('DESTROYED', event);
           const data = {
             sessionId: this.sessionHelper.session.sessionId,
             connection: {
@@ -89,31 +64,14 @@ export default class Home extends Component {
           let index = newData.indexOf(event.connection.id)
           newData.splice(index, 1)
           this.setState({connectedUsers: newData})
-
-          console.log('data is', data);
-          // fetch(`/api/event`, {
-          //   body: JSON.stringify(data), // must match 'Content-Type' header
-          //   cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-          //   credentials: 'same-origin', // include, same-origin, *omit
-          //   headers: {
-          //     'user-agent': 'Mozilla/4.0 MDN Example',
-          //     'content-type': 'application/json'
-          //   },
-          //   method: 'POST', // *GET, POST, PUT, DELETE, etc.
-          //   mode: 'cors', // no-cors, cors, *same-origin
-          //   redirect: 'follow', // manual, *follow, error
-          //   referrer: 'no-referrer', // *client, no-referrer
-          // });
-          // this.refreshSession();
         });
 
 
 
         this.sessionHelper.session.on("connectionCreated", (event) => {
-          console.log('CREATED', event);
           const updatedConnectionCount = this.state.totalConnectionsCreated + 1
           this.setState({totalConnectionsCreated: updatedConnectionCount})
-          console.log('**** Total connections: ' + this.state.totalConnectionsCreated)
+
           const data = {
             sessionId: this.sessionHelper.session.sessionId,
             connection: {
@@ -123,39 +81,16 @@ export default class Home extends Component {
           }
 
           this.setState({ connectedUsers: [...this.state.connectedUsers, event.connection.id] })
-          console.log('data is', data);
-          // fetch(`/api/event`, {
-          //   body: JSON.stringify(data), // must match 'Content-Type' header
-          //   cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-          //   credentials: 'same-origin', // include, same-origin, *omit
-          //   headers: {
-          //     'user-agent': 'Mozilla/4.0 MDN Example',
-          //     'content-type': 'application/json'
-          //   },
-          //   method: 'POST', // *GET, POST, PUT, DELETE, etc.
-          //   mode: 'cors', // no-cors, cors, *same-origin
-          //   redirect: 'follow', // manual, *follow, error
-          //   referrer: 'no-referrer', // *client, no-referrer
-          // });
-          // this.refreshSession();
         });
 
 
 
         this.sessionHelper.session.on("signal", (event) => {
-          console.log("Signal sent from connection ", event);
-          console.log("Signal type", event.type);
-          // this.refreshSession(); // FIXME Error: home.js:110 Uncaught TypeError: _this2.refreshSession is not a function
-
           if (event.type === 'signal:displayJourneyRequest') {
-            console.log("**** CAPTURED the journey request !! ")
-            console.log(event)
-            let data = JSON.parse(event.data)
-
             this.setState({
               displayMessageVisible: true,
-              displayMessageText: data.description,
-              sessionUrl: data.messageUrl
+              displayMessageText: "George has created a session 'Daily Jetsons Meditation'.", //TEMP hard coded
+              sessionUrl: '/another-jetsons-url'
             });
           }
         });
@@ -172,27 +107,14 @@ export default class Home extends Component {
         state.journeys = json;
       });
 
-    fetch('/api/active_journeys')
-      .then(res => res.json())
-      .then(json => {
-        state.activeJourneys = json
-      });
-
-
   }
-
-
-
-
 
   render() {
     return (
-      <div className="home container">
-        <div className="row">
-          <span className="col-sm"><UserList userCount={this.state.totalConnectionsCreated} userIds={this.state.connectedUsers} /></span>
-          <span className="col-sm"><EventMessage journeys={state.activeJourneys} sessionUrl={this.state.sessionUrl} /></span>
-          <span className="col-sm"><GeneratorForm /></span>
-        </div>
+      <div className="home">
+        <UserList userCount={this.state.totalConnectionsCreated} userIds={this.state.connectedUsers} />
+        <EventMessage journeyse={state.journeys} />
+        <GeneratorForm />
       </div>
       )
   }
