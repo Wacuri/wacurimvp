3b1ad5f8a87ad753a2d1488696e6701f4b1e8840
Raphael Villas <raphael.villas@gmail.com>
2018-06-12 21:52:19 -0500

    Cleanup

--- client/src/components/countdown_message.js
+++ client/src/components/countdown_message.js
@@ -4,29 +4,13 @@ class CountdownMessage extends Component {
   constructor(props) {
     super(props);
 
-    // var deadline = Date.parse(props.endTime);
-    // var now = Date.parse(new Date());
-    // var timeRemaining = deadline - now
-
-    // var seconds = Math.floor( (timeRemaining/1000) % 60 );
-    // var minutes = Math.floor( (timeRemaining/1000/60) % 60 );
-    // var hours = Math.floor( (timeRemaining/(1000*60*60)) % 24 );
-    // var days = Math.floor( timeRemaining/(1000*60*60*24) );
-
     this.state = {
       currentCount: 10,
-      // timeValues: {
-        // 'total': timeRemaining,
-        // 'days': days,
-        // 'hours': hours,
-        // 'minutes': minutes,
-        // 'seconds': seconds
       total: '',
       days: '',
       hours: '',
       minutes: '',
       seconds: ''
-      // }
     }
 
     this.countdown = this.countdown.bind(this);
@@ -34,18 +18,6 @@ class CountdownMessage extends Component {
 
   componentDidMount() {
     var intervalId = setInterval(this.countdown, 1000);
-
-    // var timeCounter = this.refs.countdown_ref;
-
-    // if(this.state.timeValues.minutes<=5){
-    //   setInterval(function(){
-    //     timeCounter.innerHTML = 'minutes: ' + t.minutes + '<br>' +
-    //                       'seconds: ' + t.seconds;
-    //     if(this.state.timeValues.seconds<=0){
-    //       clearInterval(timeinterval);
-    //     }
-    //   },1000);
-    // }
   }
 
   countdown() {
