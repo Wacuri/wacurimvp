b5642efbe26dd037b342c4f8b7eb28b12c613572
Robert L. Read <read.robert@gmail.com>
2018-09-02 17:27:08 -0500

    centering logo

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -5,8 +5,8 @@ import state from '../state';
 import logo from 'file-loader!isomorphic-loader!../../res/images/CuriousLive-logo.png';
 
 const Header = () => (
-  <div className="header" style={{display: 'flex', alignItems: 'center'}}>
-    <h1 className='logo'><img src={logo}/></h1>
+  <div className="header">
+    <img className="logo" src={logo}/>
     {state.journey && !state.journey.startAt &&
       <h2 style={{color: 'white', fontSize: '16px'}}>{state.journey.name}</h2>
     }
