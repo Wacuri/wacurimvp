9833efa0b600772811dffa137d1bf3d77bd84878
Robert L. Read <read.robert@gmail.com>
2018-10-21 21:48:11 -0500

    improvements in the banner

--- client/src/app.js
+++ client/src/app.js
@@ -294,17 +294,27 @@ class JourneyBoard extends Component {
 		<LTB.JourneyBoardBar history={this.props.history} showLeave={false} showOrientation={true}
 	          onOrientation={this.onOrientation}
 		/>
-                <div className="jb-banner">
-                     <div  className="banner-text">
-                        <h1>The CuriousLive JourneyBoard</h1>
-                        <p>Welcome to the CuriousLive JourneyBoard
-                          "Wake Up Curious" &hellip; It's the five-minute mental day spa everybody's talking about.
-                        </p>
-                         <p>
-                           Select a journey to begin whenever you are ready. Wait for others to join, or invite friends!
-                         </p>
+
+            {/*
+			<div class="main-banner">
+				<div class="container">
+					<h1>The CuriousLive JourneyBoard</h1>
+					<p>Welcome to the CuriousLive JourneyBoard "Wake Up Curious" … It's the five-minute mental day spa everybody's talking about.</p>
+					<p>Select a journey to begin whenever you are ready. Wait for others to join, or invite friends!</p>
+		</div>
+                </div>
+             */}
+
+            {/*		<div class="main-header clearfix">
+                   <img  src={journeyboardbannerimage}/>                
+		</div>
+             */}
+                <div className="main-banner" style={{backgroundImage: `url(${journeyboardbannerimage})`}}>
+                     <div  className="container">
+		          <h1>The CuriousLive JourneyBoard</h1>
+					<p>Welcome to the CuriousLive JourneyBoard "Wake Up Curious" … It's the five-minute mental day spa everybody's talking about.</p>
+					<p>Select a journey to begin whenever you are ready. Wait for others to join, or invite friends!</p>
                      </div>
-                     <img  src={journeyboardbannerimage}/>                
                 </div>
       <div className='joinable-journeys'>
         {state.joinableJourneys.map(journey => <JoinableJourneyCard key={journey._id+"_"+discriminator++} journey={journey} audioTag={this.audioTag}/>)}
